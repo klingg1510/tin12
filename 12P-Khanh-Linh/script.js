553af24588527f25f755974678ba9f780a832ea5
klingg1510 <lekhanhlinh1510@gmail.com>
2025-02-26 09:14:49 +0700

    Update script.js

--- 12P-Khanh-Linh/script.js
+++ 12P-Khanh-Linh/script.js
@@ -1,3 +1,12 @@
 function login()
-{
+{ 
+  let a = username.value
+  let b = password.value
+  if (a == "lin.klingg" && b == "lin.klingg2007")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+  else document.write("Không hợp lệ")
 }
+
